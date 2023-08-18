0c9917d4730e1822a21ea77d356143dc65b3d2d7
hsu-hsu <hsuyethin@gmail.com>
2023-08-18 17:55:38 +0700

    add chain to button

--- app/src/main/java/android/example/com/colormyviews/MainActivity.kt
+++ app/src/main/java/android/example/com/colormyviews/MainActivity.kt
@@ -19,10 +19,11 @@ class MainActivity : AppCompatActivity() {
     private fun setlisterner() {
         val clickableViews: List<View> =
             listOf(binding.boxOneText, binding.boxTwoText, binding.boxThreeText,
-                binding.boxFourText, binding.boxFiveText)
+                binding.boxFourText, binding.boxFiveText, binding.greenButton, binding.redButton,
+                binding.yellowButton)
 
         for (item in clickableViews) {
-            item.setOnClickListener { makeColored(it) }
+            item.setOnClickListener { makeColored(it)}
         }
     }
 
@@ -37,6 +38,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_button -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> binding.boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> binding.boxFiveText.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
